a5b8e2f61c342ed2d99f37ee057785975f93d0bc
izumimatsuo <gurux2daddy@gmail.com>
2019-03-07 23:34:25 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ansible-role-rkhunter [![Build Status](https://travis-ci.org/izumimatsuo/ansible-role-rkhunter.svg?branch=master)](https://travis-ci.org/izumimatsuo/ansible-role-rkhunter)
+
+CentOS 7 に rkhunter を構築する ansible role です。
+
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,12 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_rkhunter_is_installed(host):
+    package = host.package("rkhunter")
+    assert package.is_installed
+    assert package.version.startswith("1.4")
