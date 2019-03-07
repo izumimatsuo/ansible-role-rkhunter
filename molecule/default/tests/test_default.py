6972e00675fea93447418c28b584fe06bc3af611
izumimatsuo <gurux2daddy@gmail.com>
2019-03-08 00:21:13 +0900

    add testcase

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,3 +10,7 @@ def test_rkhunter_is_installed(host):
     package = host.package("rkhunter")
     assert package.is_installed
     assert package.version.startswith("1.4")
+
+
+def test_rkhunter_is_check_successful(host):
+    assert host.run_test("rkhunter --check --sk --rwo")
